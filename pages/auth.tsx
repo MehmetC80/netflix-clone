975c6e845403a2cd7553019a4e8a04696323502a
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-04-22 10:44:05 +0200

    add Input.tsx update auth.tsx

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,10 @@
+import Input from '@/components/ui/Input';
+import { useState } from 'react';
+
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div
       className="
@@ -50,7 +56,29 @@ const Auth = () => {
             gap-4
             
             '
-            ></div>
+            >
+              <Input
+                label='Email'
+                onChange={(e: any) => setEmail(e.target.value)}
+                id='email'
+                type='email'
+                value={email}
+              />
+              <Input
+                label='Username'
+                onChange={(e: any) => setName(e.target.value)}
+                id='name'
+                type='text'
+                value={name}
+              />
+              <Input
+                label='Password'
+                onChange={(e: any) => setPassword(e.target.value)}
+                id='password'
+                type='password'
+                value={password}
+              />
+            </div>
           </div>
         </div>
       </div>
